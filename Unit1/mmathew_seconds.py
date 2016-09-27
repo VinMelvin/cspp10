e1715fa4ef6983c2a7968e07935c1d324dc23567
Melvin Mathew <melvinmathew@uagateway.org>
2016-09-27 16:16:42 +0000

    Finished second Lab

--- Unit1/mmathew_seconds.py
+++ Unit1/mmathew_seconds.py
@@ -5,4 +5,4 @@ y = int(seconds) - x_seconds
 minutes = y / 60 
 c = int(minutes) % 60
 hour= int(minutes) - c
-print (int(seconds) + "seconds is" + int(hour) + "hours," + int(minutes) + "minute, and" + int(x_seconds) + "seconds")
+print (str(seconds) + " seconds is " + str(hour) + " hours, " + str(minutes) + " minute, and "  + str(x_seconds) + " seconds")
