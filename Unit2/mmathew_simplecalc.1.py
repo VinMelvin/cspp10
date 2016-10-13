495a8e360d5ffcc2cffe2ab4b98ded774427c4c0
Melvin Mathew <melvinmathew@uagateway.org>
2016-10-13 16:14:20 +0000

    Takeing it Further

--- Unit2/mmathew_simplecalc.1.py
+++ Unit2/mmathew_simplecalc.1.py
@@ -1,6 +1,22 @@
+# operation = input("Enter operation: ")
+# operation_1 = int(operation[0])
+# operation_2 = int(operation[2])
+# if operation[1] == "+":
+#     print("The result is " + str(operation_1 + operation_2))
+# elif operation[1] == "-":
+#     print("The result is " + str(operation_1 - operation_2))
+# elif operation[1] == "*":
+#     print("The result is " + str(operation_1 * operation_2))
+# elif operation[1] == "/":
+#     print("The result is " + str(operation_1 / operation_2))
+# elif operation[1] == "%":
+#     print("The result is " + str(operation_1 % operation_2))
+# else:
+#     print ("error")
+
 operation = input("Enter operation: ")
-operation_1 = int(operation[0])
-operation_2 = int(operation[2])
+operation_1 = input("Enter the length of the first term in terms of digits: ")
+operation_2 = input("Enter the length of the second term in terms of digits: ")
 if operation[1] == "+":
     print("The result is " + str(operation_1 + operation_2))
 elif operation[1] == "-":
@@ -13,3 +29,7 @@ elif operation[1] == "%":
     print("The result is " + str(operation_1 % operation_2))
 else:
     print ("error")
+
+
+operation[0:operation_1] + operation[
+    
