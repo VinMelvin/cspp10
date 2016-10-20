139f7224387bf51dde819af375bab7d5e2bbbafe
Melvin Mathew <melvinmathew@uagateway.org>
2016-10-20 00:10:49 +0000

    Code Check On Unit2 Lab 1 and 2

--- Unit2/mmathew_simplecalc.1.py
+++ Unit2/mmathew_simplecalc.1.py
@@ -1,22 +1,6 @@
-# operation = input("Enter operation: ")
-# operation_1 = int(operation[0])
-# operation_2 = int(operation[2])
-# if operation[1] == "+":
-#     print("The result is " + str(operation_1 + operation_2))
-# elif operation[1] == "-":
-#     print("The result is " + str(operation_1 - operation_2))
-# elif operation[1] == "*":
-#     print("The result is " + str(operation_1 * operation_2))
-# elif operation[1] == "/":
-#     print("The result is " + str(operation_1 / operation_2))
-# elif operation[1] == "%":
-#     print("The result is " + str(operation_1 % operation_2))
-# else:
-#     print ("error")
-
 operation = input("Enter operation: ")
-operation_1 = input("Enter the length of the first term in terms of digits: ")
-operation_2 = input("Enter the length of the second term in terms of digits: ")
+operation_1 = int(operation[0])
+operation_2 = int(operation[2])
 if operation[1] == "+":
     print("The result is " + str(operation_1 + operation_2))
 elif operation[1] == "-":
@@ -30,6 +14,26 @@ elif operation[1] == "%":
 else:
     print ("error")
 
+#Taking It Futher
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+# expression = input("Enter expression: ")
+# term_1 = int(input("Enter the length of the first term in terms of digits: "))
+# term_2 = int(input("Enter the length of the second term in terms of digits: "))
+# term_3 = term_1 - 1
+# if expression[term_1] == "+":
+#     print("The result is " + int(expression[0:term_1]) + str(expression[term_3:]))
+# elif expression[term_1] == "-":
+#     print("The result is " + int(expression[0:term_1] - expression[term_3:]))
+# elif expression[term_1] == "*":
+#     print("The result is " + int(expression[0:term_1] * expression[term_3:]))
+# elif expression[term_1] == "/":
+#     print("The result is " + int(expression[0:term_1] / expression[term_3:]))
+# elif expression[term_1] == "%":
+#     print("The result is " + int(expression[0:term_1] % expression[term_3:]))
+# else:
+#     print ("error")
 
-operation[0:operation_1] + operation[
+
+
     
