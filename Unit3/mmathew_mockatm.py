389ecfeab83aa2be942168462951fc048b4251c2
Melvin Mathew <melvinmathew@uagateway.org>
2016-11-01 16:11:30 +0000

    Done With Number Guessing Game

--- Unit3/mmathew_mockatm.py
+++ Unit3/mmathew_mockatm.py
@@ -1,18 +1,19 @@
 bank_account = 10000
 print("1. Withdraw \n2. Deposit \n3. Exit")
-choice = int(input("Welcome to ATM! Pick from above [1|2|3]: "))
-while choice != "3":#student completes while loop
-    if choice == "1":#user chooses 'withdraw'
-        amount = int(input("How much to withdraw: "))
-        if amount > bank_account:
-            print("withdraw is too much")
-        else:
-            print(bank_account == bank_account - amount) #student does this part
+choice = input("Welcome to ATM! Pick from above [1|2|3]: ")
+while choice != "3":
+    if choice == "1":
+        withdraw = int(input("How much to withdraw: "))
+        bank_account = bank_account - withdraw
+        print(str(bank_account))
     elif choice == "2":
         amount = int(input("How much to deposit: "))
-        print(bank_account == bank_account + amount)
-    elif choice == "3": #user chooses 'exit'
-        print("goodbye")#student does this part too
-
+        bank_account = bank_account + amount
+        print(bank_account)
+    elif choice == "3":
+        print("goodbye")
+        
+        
     print("1. Withdraw \n2. Deposit \n3. Exit")
     choice = input("Pick from above [1|2|3]:")
+    
--- Unit3/mmathew_numguess.py
+++ Unit3/mmathew_numguess.py
@@ -1,4 +1,16 @@
-num_guess = int(input("Enter Guess Number 1 - 100 : "))
-if num_guess < 24:
-    print("It's way too low.")
-    
+import random
+num_guess = input("Enter Guess Number 1 - 100 : ")
+num = random.randint(1,101)
+num_times = 1
+while num_guess != num:
+    num_times = num_times + 1
+    if num_guess < num:
+        print("Too low! Try again.")
+    elif num_guess > num:
+        print("Too High! Try again.")
+    elif num_guess == num:
+        print("You got It")
+    num_guess = int(input("Enter Guess Number 1 - 100 : "))
+
+print("You got It")
+print("Number of Tries " + str(num_times))
