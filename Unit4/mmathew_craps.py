d5fee2ba04b2866a8c2f45311b1e9873c2e281f8
Melvin Mathew <melvinmathew@uagateway.org>
2016-12-13 17:15:52 +0000

    Working On Craps

--- Unit4/mmathew_craps.py
+++ Unit4/mmathew_craps.py
@@ -3,17 +3,18 @@ print("Welcome To Craps\n---------------------------------------")
 
 # function name: bank_account  
 #   purpose: The amount of money in the players bank and how much to bet
-#   arguments: 
+#   arguments:   
+
 #   returns: 
-def bank_account(bet):
+def bank_account():
     bank = int(100)
     print("You have ${} in your bank".format(bank))
     bet = int(input("Enter a whole number for your bet: "))
-    if bet >= 100:
-        print("You don't have that much money!!!")
-    elif bet < 100:
-        print("Bet a postive number")
-bank_account(bet)
+#     if bet >= 100:
+#         print("You don't have that much money!!!")
+#     elif bet < 0:
+#         print("Bet a postive number")
+bank_account()
 
 
 
@@ -28,7 +29,7 @@ def roll2dice():
     dice_sum = dice1 + dice2    
     print("Rolled 2 dice: {} {}, Total of {}".format(dice1,dice2,dice_sum))
     return dice_sum
-
+roll2dice()
 # function name: first_roll_result
 #   purpose: get the result of the first roll
 #   arguments: roll - the sum of the two dice rolled
@@ -37,13 +38,14 @@ def roll2dice():
 #       if roll is 2,3,12: return "lose"
 #       if otherwise: return "point number"
 def first_roll_result(roll):
+    roll = roll2dice()
     if roll == 7 or 11:
         return "win"
     elif roll == 2 or 3 or 12:
         return "lose"
     else:
         return "point number"
-
+first_roll_result(roll)
 # function name: bank_account  
 #   purpose: 
 #   arguments: 
