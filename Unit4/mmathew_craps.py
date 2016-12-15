cfd0ff320f018223bad8972f3e326d990ca9af69
Melvin Mathew <melvinmathew@uagateway.org>
2016-12-15 17:14:43 +0000

    Working On Lab 2

--- Unit4/mmathew_craps.py
+++ Unit4/mmathew_craps.py
@@ -6,15 +6,17 @@ print("Welcome To Craps\n---------------------------------------")
 #   arguments:   
 
 #   returns: 
-def bank_account():
+def bank_account(bank,bet):
     bank = int(100)
     print("You have ${} in your bank".format(bank))
     bet = int(input("Enter a whole number for your bet: "))
+    return bet
 #     if bet >= 100:
 #         print("You don't have that much money!!!")
 #     elif bet < 0:
 #         print("Bet a postive number")
-bank_account()
+
+print(bank_account())
 
 
 
@@ -29,7 +31,7 @@ def roll2dice():
     dice_sum = dice1 + dice2    
     print("Rolled 2 dice: {} {}, Total of {}".format(dice1,dice2,dice_sum))
     return dice_sum
-roll2dice()
+
 # function name: first_roll_result
 #   purpose: get the result of the first roll
 #   arguments: roll - the sum of the two dice rolled
@@ -37,18 +39,22 @@ roll2dice()
 #       if roll is 7,11: return "win"
 #       if roll is 2,3,12: return "lose"
 #       if otherwise: return "point number"
-def first_roll_result():
-    roll = roll2dice
+def first_roll_result(roll):
     if roll == 7 or roll == 11:
         return "win"
     elif roll == 2 or roll == 3 or roll == 12:
         return "lose"
     else:
         return "point number"
-first_roll_result()
+
+dice_total = roll2dice()
+print(first_roll_result(dice_total))
 # function name: bank_account  
 #   purpose: 
 #   arguments: 
 #   returns: 
 
-input("Press l to terminate.")
+input("Press Enter to terminate.")
+
+#def house():
+    #if 
