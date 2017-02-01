f1ee51f125326025c3b0a72012ac99db366bee79
Melvin Mathew <melvinmathew@uagateway.org>
2017-02-01 17:15:29 +0000

    Inteacting the first and last letters

---
+++ Unit5/mmathew_scramble.py
@@ -0,0 +1,29 @@
+
+phrase = "I love learning computer science"
+def scramble_word():
+    
+    
+    
+    
+    
+    
+def scramble_phrase():
+    
+    
+    
+    import random
+
+word = "hello"
+print(list(word[1:-1]))
+
+some_list = [1,2,3,4]
+random.shuffle(some_list)
+print(some_list)
+
+sentence = "ello everyone in the class"
+#print(sentence.split(" "))
+print(list(sentence[1:-1]))
+
+
+word = "hello"
+print(word[1:-1])
