13ebe2a5e6d633e9dfc075a809c20001cfc6fa87
Melvin Mathew <melvinmathew@uagateway.org>
2017-02-02 17:13:52 +0000

    Scramble

--- Unit5/mmathew_scramble.py
+++ Unit5/mmathew_scramble.py
@@ -1,29 +1,16 @@
-
-phrase = "I love learning computer science"
-def scramble_word():
+#import random
+word = "Melvin"
+# funcation to shuffles word
+#def scramble_word():
+    # split word by list
+print(list(word))
     
     
     
     
     
     
-def scramble_phrase():
+#def scramble_phrase():
     
     
     
-    import random
-
-word = "hello"
-print(list(word[1:-1]))
-
-some_list = [1,2,3,4]
-random.shuffle(some_list)
-print(some_list)
-
-sentence = "ello everyone in the class"
-#print(sentence.split(" "))
-print(list(sentence[1:-1]))
-
-
-word = "hello"
-print(word[1:-1])
--- Unit5/test.py
+++ Unit5/test.py
@@ -1,2 +1,19 @@
+# word = "hello"
+# num = "what your name "
+# print(word[1:-1])
+import random
+word = "hello"
+print(list(word[1:-1]))
+
+some_list = [1,2,3,4]
+random.shuffle(some_list)
+print(some_list)
+
+sentence = "ello everyone in the class"
+print(sentence.split(" "))
+# print(sentence[1:-1])
+for x in range(sentence):
+    print(sentence[1:-1])
+
 word = "hello"
 print(word[1:-1])
