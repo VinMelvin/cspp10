81ec8eca6e480242458c2ee5d04d38930647497f
Melvin Mathew <melvinmathew@uagateway.org>
2017-02-16 17:12:20 +0000

    Working On add Fuction

--- Unit5/dictionary.py
+++ Unit5/dictionary.py
@@ -5,17 +5,24 @@ diction = {
     
 }
 
-def add(key):
+def add():
     user_input = input("What key do you want to add? ")
     Key_value = input("What is the value of the key? ")
     diction[user_input] = Key_value
     pprint(diction)
-    for key 
+    for user_input in diction:
+        if diction[user_input] == Key_value:
+            print("exists as value! TRY AGAIN") 
+            add()
+    else:
+        main_fun()
 
-
-key = input("what would you like to do? ")
-if key == "add":
-    add(key)
+def main_fun():
+    key = input("what would you like to do? ")
+    if key == "add":
+        add()
+        
+main_fun()
     
     
     
